211d2be4b1601aa18bf345c8103a6b9f5e49bec8
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-03-31 06:00:34 +0530

    @Value annotion is used for use application properties  values (use only single value in single field

--- Practice/src/main/java/com/SpringBoot/Practice/test/EmailSender.java
+++ Practice/src/main/java/com/SpringBoot/Practice/test/EmailSender.java
@@ -1,14 +1,19 @@
 package com.SpringBoot.Practice.test;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class EmailSender {
     public String emailProvider;
+
+    @Value("${app.email.message}")
+    public String emailMessage;
 
     public EmailSender(String emailProvider) {
         this.emailProvider = emailProvider;
     }
 
     public String sendEmail() {
-        return "Email sent using " + emailProvider;
+        return "Email sent using " + emailProvider + " with message: " + emailMessage;
     }
 
 }
--- Practice/src/main/java/com/SpringBoot/Practice/test/config.java
+++ Practice/src/main/java/com/SpringBoot/Practice/test/config.java
@@ -1,14 +1,18 @@
 package com.SpringBoot.Practice.test;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class config {
 
+    @Value("${app.email.provider}")
+    private String emailProvider;
+
     @Bean
     public EmailSender emailSender() {
-        return new EmailSender("Gmail");
+        return new EmailSender(emailProvider);
     }
 
     @Bean
